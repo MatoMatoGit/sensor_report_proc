c7a777d57ce34a4ba74d57b3a22a3f9d2fc5c224
Dorus <spijker.dorus@gmail.com>
2020-02-10 14:06:26 +0100

    + main: Parse inputs arguments, pass message list JSON. + main: Create message loaders for each message type. + main: Type and Timestamp added to the query.

--- main.py
+++ main.py
@@ -1,18 +1,42 @@
 from common.MessageFileLoader import MessageFileLoader
+# from gql import gql, Client
+# from gql.transport.requests import RequestsHTTPTransport
 import time
-from gql import gql, Client
-from gql.transport.requests import RequestsHTTPTransport
-
-_transport = RequestsHTTPTransport(
-    url='http://localhost:5000/graphql',
-    use_json=True,
-)
+import sys
+import getopt
+import json
 
 
-def MessageDataToQuery(data, id, timestamp):
-    return gql("""
+# _Transport = RequestsHTTPTransport(
+#     url='http://localhost:5000/graphql',
+#     use_json=True,
+# )
+
+_MsgListFile = None
+_MsgList = {}
+_BaseDir = "./"
+
+
+def ComposePath(base, type):
+    return base + "/" + str(type)
+
+
+# mutation {
+#   createMeasurement(sensorHash:"bbb", sensorType:"MOIST", data:12, createdOnModule:"12u84872"){
+#     measurement {
+#       id
+#     }
+#   }
+# }
+
+def MessageDataToQuery(data, id, msg_type, timestamp):
+    print(id)
+    print(msg_type)
+    print(timestamp)
+    print(data)
+    return print("""
     mutation {
-      createMeasurement(data:""" + str(data) + """, sensorHash: \"""" + str(id) + """\"){
+      createMeasurement(data:""" + str(data) + """, sensorHash: \"""" + str(id) + """\", sensorType: \"""" + msg_type + """\",  createdOnModule: \"""" + timestamp + """\"{
         sensor {
           id
           sensorHash
@@ -33,6 +57,16 @@ def VerifyId(msg_meta, tx_id):
     return False
 
 
+def MessageTypeToString(msg_type):
+    print(msg_type)
+    global _MsgList
+
+    for entry in _MsgList:
+        if _MsgList[entry]["type"] == msg_type:
+            return entry
+    return None
+
+
 def ProcessMessage(client, msg, id):
     if VerifyMessage(msg, msg) is False:
         print("[MsgProc] Error: Received message does not match the defined schema.")
@@ -43,35 +77,82 @@ def ProcessMessage(client, msg, id):
         return
 
     datetime = msg["meta"]["dt"]
+    msg_type = str(msg["meta"]["type"])
+    msg_stype = str(msg["meta"]["stype"])
     samples = msg["data"]["S"]
+
+    msg_type = msg_type + '.' + msg_stype
+    msg_type_str = MessageTypeToString(msg_type)
+
+    if msg_type_str is None:
+        return -1
+
     for s in samples:
-        query = MessageDataToQuery(s, id, datetime)
-        print(client.execute(query))
+        query = MessageDataToQuery(s, id, msg_type_str, datetime)
+        #print(client.execute(query))
+
+    return 0
 
 
-def main(directory):
+def ProcessInputsArgs(argv):
+    global _MsgListFile
+    global _BaseDir
 
-    msg_loader = MessageFileLoader(directory)
+    try:
+        opts, args = getopt.getopt(argv, "hl:d:", ["list=", "dir="])
+    except getopt.GetoptError:
+        print('-l <list_file>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('-l <list_file>')
+            sys.exit()
+        elif opt in ("-l", "--list"):
+            _MsgListFile = arg
+        elif opt in ("-d", "--dir"):
+            _BaseDir = arg
+    print('Message list file is "', _MsgListFile)
+    time.sleep(2)
 
-    client = Client(
-        transport=_transport,
-        fetch_schema_from_transport=True,
-    )
+
+def main(argv):
+    global _MsgListFile
+    global _MsgList
+    global _BaseDir
+
+    msg_loaders = set()
+
+    ProcessInputsArgs(argv)
+
+    msg_file = open(_MsgListFile, 'r')
+    msg_file_str = msg_file.read()
+    print(msg_file_str)
+    msg_file.close()
+
+    _MsgList = json.loads(msg_file_str)
+
+    for msg_entry in _MsgList.keys():
+        print(msg_entry)
+        path = ComposePath(_BaseDir, _MsgList[msg_entry]["type"])
+        msg_loaders.add(MessageFileLoader(path))
+
+    # client = Client(
+    #     transport=_Transport,
+    #     fetch_schema_from_Transport=True,
+    # )
+
+    client = None
 
     while True:
         while True:
-            tx_id, seq_nr, msg = msg_loader.LoadNext()
-
-            if seq_nr != -1:
-                ProcessMessage(client, msg, tx_id)
-                msg_loader.Remove(tx_id, seq_nr)
-            else:
-                print("No more files available")
-                break
-
-        time.sleep(5)
+            for msg_loader in msg_loaders:
+                tx_id, seq_nr, msg = msg_loader.LoadNext()
+                if seq_nr != -1:
+                    ProcessMessage(client, msg, tx_id)
+                    msg_loader.Remove(tx_id, seq_nr)
+            time.sleep(2)
 
 
 if __name__ == '__main__':
-    main("../upyiot/test/0.2")
+    main(sys.argv[1:])
 
